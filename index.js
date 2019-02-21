3c9bada236a6d7c654a890126ad5e1bbb613b8e4
thisisfruit <42990686+thisisfruit@users.noreply.github.com>
2019-02-21 11:34:25 +0800

    e-commerce bot

--- index.js
+++ index.js
@@ -10,63 +10,63 @@ var app = express();
 var port = process.env.PORT || process.env.port || 5000;
 app.set('port',port);
 app.use(bodyParser.json());
+app.use(express.static('public'));
 
-const MOVIE_APP_ID =config.get('movie_app_id');
+const SHEETDB_PRODUCTINFO_ID =config.get('productinfo_id');
 
 app.post('/webhook',function(req, res){
+    console.log("[WebHook] In");
     let data = req.body;
-    let MovieName = data.queryResult.parameters.MovieName;
-    let propertiesObject = {
-        api_key:MOVIE_APP_ID,
-        query:MovieName,
-        language:"zh-TW"
-    };
-    request({
-        uri:'https://api.themoviedb.org/3/search/movie?',
-        json:true,
-        qs:propertiesObject
-    },function(error, response, body){
-        if(!error && response.statusCode ==200){
-            var thisFulfullmentMessages = [];
-            
-            for (var i=0;i<body.results.length;i++){
-
-                    var thisText={};
-                    thisText.text={};
-                    thisText.text.text=[];
-                    thisText.text.text.push(body.results[i].title);
-
-                    var thisText2={};
-                    thisText2.text={};
-                    thisText2.text.text=[];
-                    thisText2.text.text.push(body.results[i].overview);
-
-                    var thisImage={};
-                    thisImage.image={};
-                    thisImage.image.imageUri='https://image.tmdb.org/t/p/w300_and_h450_bestv2'+ body.results[i].poster_path;
-
-                    // var thisObject={};
-                    // thisObject.card={};
-                    // thisObject.card.title=body.results[i].title;
-                    // thisObject.card.subtitle=body.results[i].overview;
-                    // thisObject.card.imageUri='https://image.tmdb.org/t/p/w300_and_h450_bestv2'+ body.results[i].poster_path;
-                    // thisObject.card.buttons=[{
-                    //     "text":"上映時間:" + body.results[i].release_date + ' (點我看預告片) ',
-                    //     "postback":'https://www.themoviedb.org/movie/' + body.results[i].id +'/videos?active_nav_item=Trailers&video_language=en-US'
-                    // }];
-
-                    thisFulfullmentMessages.push(thisText,thisText2,thisImage); //,thisObject
-                    var responseObject = {fulfillmentMessages:thisFulfullmentMessages};
-                };
-                res.json(responseObject);
-            }else{
-        console.log("[theMovieDB] failed")
+    let queryCategory = data.queryResult.parameters["Category"];
+    console.log("[queryCategory] "+queryCategory);
+    var thisQs = {};
+    if(queryCategory == "熱門"){
+        thisQs.IsHot = "TRUE";
+    }else{
+        thisQs.Category =queryCategory;
     }
-});
+    thisQs.casesensitive = false;
+    
+    request({
+        uri:"https://sheetdb.io/api/v1/"+SHEETDB_PRODUCTINFO_ID+"/search?",
+        json:true,
+        method:"GET",
+        headers:{"Content-Type":"application/json"},
+        qs:thisQs
+    },function(error, response, body){
+
+        if(!error && response.statusCode ==200){
+            console.log("[SheetDB API] Success");
+            sendCards(body, res);
+        }else{
+            console.log("[SheetDB API] failed");
+        }
+    });        
 });
 
 app.listen(app.get('port'),function(){
     console.log('[app.listen] Node app is running on port', app.get('port'));
-})
+});
 
 module.exports = app;
+
+function sendCards(body, res){
+    
+    console.log("[send] In");
+    var thisFulfillmentMessages = [];
+            for (var i=0;i<body.length;i++){
+                    
+                var thisObject={};
+                    thisObject.card={};
+                    thisObject.card.title=body[i].Name;
+                    thisObject.card.subtitle=body[i].Category;
+                    thisObject.card.imageUri=body[i].Photo;
+                    thisObject.card.buttons=[{
+                        "text":"看大圖",
+                        "postback":body[i].Photo
+                    }];
+                    thisFulfillmentMessages.push(thisObject);
+                }
+                    var responseObject = {fulfillmentMessages:thisFulfillmentMessages};
+                res.json(responseObject);
+            };
